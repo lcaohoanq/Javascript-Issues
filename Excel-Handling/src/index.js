bf9a0805bb3caef6f00be39b59257b48d3b074f2
lcaohoanq <hoangclw@gmail.com>
2024-04-27 08:05:53 +0700

    feat: add check is exist column

--- Excel-Handling/src/index.js
+++ Excel-Handling/src/index.js
@@ -1,6 +1,6 @@
 const XLSX = require("xlsx");
 
-const filePath = "./Products Blank Cell Dreame.en.xlsx";
+const filePath = "./Products Dreame.en.xlsx";
 
 const workbook = XLSX.readFile(filePath);
 const sheetName = workbook.SheetNames[0];
@@ -11,12 +11,25 @@ let data = XLSX.utils.sheet_to_json(sheet, {
   blankrows: true,
 });
 
-const firstColumn = data.map((row) => row["id"]);
+function isExistColumn(column) {
+  //check if the column is exist in the first row
+  return data[0].hasOwnProperty(column);
+}
 
-// data = XLSX.utils.sheet_to_json(sheet, { header: 1, blankrows: true });
-// const secondColumn = data.map((row) => row[0]);
+function printData(column) {
+  if (isExistColumn(column)) {
+    const firstColumn = data.map((row) => row[column]);
 
-console.log(firstColumn);
-// console.log(secondColumn);
+    // data = XLSX.utils.sheet_to_json(sheet, { header: 1, blankrows: true });
+    // const secondColumn = data.map((row) => row[0]);
 
-console.log(data);
+    console.log(firstColumn);
+    // console.log(secondColumn);
+
+    console.log(data);
+  } else {
+    console.log("Column not found");
+  }
+}
+
+printData("Manufacturer");
