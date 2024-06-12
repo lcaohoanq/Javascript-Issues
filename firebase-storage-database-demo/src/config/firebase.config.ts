527903a414d3974ed3a2d7e7bcd497651dfaea63
kde <hoangclw@gmail.com>
2024-06-12 10:42:51 +0700

    chore: add just the field data of firebase

--- firebase-storage-database-demo/src/config/firebase.config.ts
+++ firebase-storage-database-demo/src/config/firebase.config.ts
@@ -6,7 +6,7 @@ export default {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     projectId: process.env.PROJECT_ID,
-    databaseURL: process.env.FIRESTORE_DB_URL,
+    databaseURL: process.env.DATABASE_URL,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
